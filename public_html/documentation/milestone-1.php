ad23e241fc4130d6ddc089472157b540d864b746
Tim Williams <tikytimbo5@yahoo.com>
2017-07-14 12:18:29 -0600

    added audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,5 +9,10 @@
 			Idealy I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
 
 		<h2>Audience</h2>
+		<p>I would like to promote myself to anyone looking for a developer.</p>
+
+		<h2>Goal</h2>
+		<p>My goal is to gain full or part time employment. i would also be willing to do an internship to get my feet wet
+			I would like to become well known in the tech community</p>
 	</body>
 </html>
