7fba2f9e8e8dc1f5c810275b4aa785f6147dfc69
Tim Williams <tikytimbo5@yahoo.com>
2017-07-14 12:29:23 -0600

    Added a persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<h2>purpose</h2>
-		<p>The purpose of Personal Web Site Project is to promote myself to possible employers.
+		<p>The purpose of my Personal Web Site Project is to promote myself to possible employers.
 			Idealy I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
 
 		<h2>Audience</h2>
@@ -14,5 +14,12 @@
 		<h2>Goal</h2>
 		<p>My goal is to gain full or part time employment. i would also be willing to do an internship to get my feet wet
 			I would like to become well known in the tech community</p>
+
+		<h2>Persona</h2>
+		Henry, 44, is the founder of a local sports magazine. Henry is looking to expand his company to include a website.
+		Henry has a nephew that is a experienced fullstack web developer but would like to add a jr developer to help with
+		the project. Having graduated from the first DDC cohort, Henry first looks for fellow graduates. Looking at sample
+		PWP projects he discovers timothymichael.org. Tim is the perfect candidate.
+		<p></p>
 	</body>
 </html>
