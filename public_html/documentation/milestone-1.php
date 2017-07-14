dbbd0f8225482cb8057a31db7900d21623dfeb0f
Tim Williams <tikytimbo5@yahoo.com>
2017-07-14 12:12:34 -0600

    Added purpose

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>timothymichael.org</title>
+	</head>
+	<body>
+		<h2>purpose</h2>
+		<p>The purpose of Personal Web Site Project is to promote myself to possible employers.
+			Idealy I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
+
+		<h2>Audience</h2>
+	</body>
+</html>
