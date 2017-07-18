c09438275a5c0e602feb9292ea59bbfd5d7808c8
Tim Williams <tikytimbo5@yahoo.com>
2017-07-17 20:34:21 -0600

    Added a persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,22 +4,25 @@
 		<title>timothymichael.org</title>
 	</head>
 	<body>
-		<h2>purpose</h2>
+		<h2>milestone 1</h2>
+		<h2>Purpose</h2>
+	</body>
 		<p>The purpose of my Personal Web Site Project is to promote myself to possible employers.
-			Idealy I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
+			Idealy as my long term goal I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
 
 		<h2>Audience</h2>
-		<p>I would like to promote myself to anyone looking for a developer.</p>
+		<p>I would like to promote myself to any potential employers.</p>
 
 		<h2>Goal</h2>
 		<p>My goal is to gain full or part time employment. i would also be willing to do an internship to get my feet wet
 			I would like to become well known in the tech community</p>
 
 		<h2>Persona</h2>
-		Henry, 44, is the founder of a local sports magazine. Henry is looking to expand his company to include a website.
-		Henry has a nephew that is a experienced fullstack web developer but would like to add a jr developer to help with
-		the project. Having graduated from the first DDC cohort, Henry first looks for fellow graduates. Looking at sample
-		PWP projects he discovers timothymichael.org. Tim is the perfect candidate.
-		<p></p>
+		<h3>Name</h3>
+		<p>Henry Ulibari</p>
+		<h2>age</h2>
+		<p>46</p>
+		<h2>Proffesion</h2>
+		<p>Henry founded a local sports Magazine covering all New Mexico sports.</p>
 	</body>
 </html>
