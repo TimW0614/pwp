a3d81bf28142b7419e2016d1f361d9b2adfa8109
Tim Williams <tikytimbo5@yahoo.com>
2017-07-25 20:30:27 -0600

    added content <p> (welcome, bio, portfolio, contact, and footer.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,12 +9,26 @@
 		<header>
 			<h1>Content Streategy</h1>
 		</header>
-		<main>
 			<h2>Overview</h2>
+
 			<p>I envision my website having 4 sections that include: My bio, A projects section that includes
 				previous projects I have been a part of (capstone, data design, etc...).
 				links to my social media (Linkedin,twitter, github), And finaly a link to my resume complete with recaptcha.</p>
-		</main>
 
+		<h2>Content</h2>
+
+		<p><em>Welcome</em>This section will welcome my viewers to my personal website</p>
+
+		<p><em>Bio</em>This section of my webpage will be all "about me." It will descriptive information about who
+		I am and my goals.</p>
+
+		<p><em>Portfolio</em>My next section will feature coding projects that I have been a part of and be used as an
+		example for employers as to why I would be good for their needs.</p>
+
+		<p><em>Contact</em>This section will have information on how to get ahold of me as well as a request form for my
+		personal resume</p>
+
+		<p><em>footer</em>At the base of my page will be links to my profesional social media accounts such as linkedin,
+		twitter and github.</p>
 	</body>
 </html>
