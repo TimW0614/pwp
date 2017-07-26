10059fd4b43fb33624adbc0c3610ce0763ab9fee
Tim Williams <tikytimbo5@yahoo.com>
2017-07-26 10:09:01 -0600

    fixed formatt

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,42 +7,43 @@
 		<h2>milestone 12</h2>
 		<h2>Purpose</h2>
 	</body>
-		<p>The purpose of my Personal Web Site Project is to promote myself to possible employers.
-			Idealy as my long term goal I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
+	<p>The purpose of my Personal Web Site Project is to promote myself to possible employers.
+		Idealy as my long term goal I would like to work for an athletic platform (ESPN, Fox Sports etc...)</p>
 
-		<h2>Audience</h2>
-		<p>I would like to promote myself to any potential employers.</p>
+	<h2>Audience</h2>
+	<p>I would like to promote myself to any potential employers.</p>
 
-		<h2>Goal</h2>
-		<p>My goal is to gain full or part time employment. i would also be willing to do an internship to get my feet wet
-			I would like to become well known in the tech community</p>
+	<h2>Goal</h2>
+	<p>My goal is to gain full or part time employment. i would also be willing to do an internship to get my feet wet
+		I would like to become well known in the tech community</p>
 
-		<h2>Persona</h2>
-		<h3>Name</h3>
-		<p>Henry Ulibari</p>
-		<h2>age</h2>
-		<p>46</p>
-		<h2>Profession</h2>
-		<p>Henry founded a local sports Magazine (NM Sports Magazine) covering all New Mexico sports.
-			Henry is looking to expand his magazine and add a website.</p>
-		<h2>Technology</h2>
-		<p>NM Sports Magazine has a headquarters in downtown Albuquerque, the office is set up with a reliable
-			high speed internet connection. All employees use macbooks and Iphones. Company also pays for cloud service</p>
-		<h2>Attitudes</h2>
-		<p>Henry is very driven with an upbeat and positive outlook, he is patient with his employees but expects the best</p>
-		<h2>Frustrations</h2>
-		<p>While Henry can be very patient, he has invested a lot to build a reputible company. Henry can grow aggitated
-			whenever deadlines are not met.</p>
-		<h2>Goals</h2>
-		<p>Henry wants to add a website to his company complete with blogs, podcasts, statistics, highlights,
-			and more. The goal is for the website to be up and running by the start of the next football season.</p>
-		<h2>Use Case</h2>
-		<p>Henrys son Brian is a graduate of the University of New Mexico. Brian attended the University on a full ride
-			athletic scholarship and majored in Computer Science. After graduating Brian was selected for an internship
-			at CBS Sports as a jr. Developer. Brian has become a well rounded Web Developer and has agreed to help his
-			dad with his website. In order to meet Henrys goals, Brian has decided to hire a jr. Developer. After attending
-			a networking event at fat pipe abq, Brian turns to DDC Bootcamp. After looking over my website it appears I may
-			be the perfect fit and he decides to call me in for a formal interview.</p>
+	<h2>Persona</h2>
+	<h3>Name</h3>
+	<p>Henry Ulibari</p>
+	<h2>age</h2>
+	<p>46</p>
+	<h2>Profession</h2>
+	<p>Henry founded a local sports Magazine (NM Sports Magazine) covering all New Mexico sports.
+		Henry is looking to expand his magazine and add a website.</p>
+	<h2>Technology</h2>
+	<p>NM Sports Magazine has a headquarters in downtown Albuquerque, the office is set up with a reliable
+		high speed internet connection. All employees use macbooks and Iphones. Company also pays for cloud service</p>
+	<h2>Attitudes</h2>
+	<p>Henry is very driven with an upbeat and positive outlook, he is patient with his employees but expects the
+		best</p>
+	<h2>Frustrations</h2>
+	<p>While Henry can be very patient, he has invested a lot to build a reputible company. Henry can grow aggitated
+		whenever deadlines are not met.</p>
+	<h2>Goals</h2>
+	<p>Henry wants to add a website to his company complete with blogs, podcasts, statistics, highlights,
+		and more. The goal is for the website to be up and running by the start of the next football season.</p>
+	<h2>Use Case</h2>
+	<p>Henrys son Brian is a graduate of the University of New Mexico. Brian attended the University on a full ride
+		athletic scholarship and majored in Computer Science. After graduating Brian was selected for an internship
+		at CBS Sports as a jr. Developer. Brian has become a well rounded Web Developer and has agreed to help his
+		dad with his website. In order to meet Henrys goals, Brian has decided to hire a jr. Developer. After attending
+		a networking event at fat pipe abq, Brian turns to DDC Bootcamp. After looking over my website it appears I may
+		be the perfect fit and he decides to call me in for a formal interview.</p>
 
 	</body>
 </html>l
--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,28 +7,31 @@
 	</head>
 	<body>
 		<header>
-			<h1>Content Streategy</h1>
+			<h1>Content Strategy</h1>
 		</header>
-			<h2>Overview</h2>
+		<h2>Overview</h2>
 
-			<p>I envision my website having 4 sections that include: My bio, A projects section that includes
-				previous projects I have been a part of (capstone, data design, etc...).
-				links to my social media (Linkedin,twitter, github), And finaly a link to my resume complete with recaptcha.</p>
+		<p>I envision my website having 4 sections that include: My bio, A projects section that includes
+			previous projects I have been a part of (capstone, data design, etc...).
+			links to my social media (Linkedin,twitter, github), And finaly a link to my resume complete with
+			recaptcha.</p>
 
 		<h2>Content</h2>
 
 		<p><em>Welcome</em><br>This section will welcome my viewers to my personal website</p>
 
-		<p><em>Bio</em><br>This section of my webpage will be all "about me." It will be descriptive information about who
-		I am and my goals.</p>
+		<p><em>Bio</em><br>This section of my webpage will be "about me." It will be descriptive information about who
+			I am and my goals.</p>
 
 		<p><em>Portfolio</em><br>My next section will feature coding projects that I have been a part of and be used as an
-		example for employers as to why I would be good for their needs.</p>
+			example for employers as to why I would be good for their needs.</p>
 
-		<p><em>Contact</em><br>This section will have information on how to get a hold of me as well as a request form for my
-		personal resume</p>
+		<p><em>Contact</em><br>This section will have information on how to get a hold of me as well as a request form for
+			my
+			personal resume</p>
 
-		<p><em>footer</em><br>At the base of my page will be links to my profesional social media accounts such as linkedin,
-		twitter and github.</p>
+		<p><em>footer</em><br>At the base of my page will be links to my profesional social media accounts such as
+			linkedin,
+			twitter and github.</p>
 	</body>
 </html>
