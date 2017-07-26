14ffacffe8e838ecdb45fd69ec80fc95eaecaa6c
Tim Williams <tikytimbo5@yahoo.com>
2017-07-25 20:09:34 -0600

    started content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,8 +1,20 @@
 <!doctype html>
 <html>
 	<head>
-		<title>
+		<meta charset="utf-8">
 
-		</title>
+		<title>Milestone-2</title>
 	</head>
+	<body>
+		<header>
+			<h1>Content Streategy</h1>
+		</header>
+		<main>
+			<h2>Overview</h2>
+			<p>I envision my website having 4 sections that include: My bio, A projects section that includes
+				previous projects I have been a part of (capstone, data design, etc...).
+				links to my social media (Linkedin,twitter, github), And finaly a link to my resume complete with recaptcha.</p>
+		</main>
+
+	</body>
 </html>
