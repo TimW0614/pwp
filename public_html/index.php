6e8c1c92a817869b27208ebed084fa83d705a2dc
Tim Williams <tikytimbo5@yahoo.com>
2017-08-13 16:04:09 -0600

    added nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -18,6 +18,7 @@
 				rel="stylesheet"/>
 
 		<!-- Custom CSS Here -->
+		<link href="css/style.css" rel="stylesheet"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -35,9 +36,37 @@
 
 		<title>timothymichael.org</title>
 	</head>
-	<body>
-		<header>
+	<a name="page-top"></a>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<!--NavBar Bootstrap Default Setting-->
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<a class="navbar-brand" href="#">Timothy Michael Williams</a>
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
 
-		</header>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="#page-top">Home <span class="sr-only">(current)</span></a></li>
+								<li><a href="#bio">Bio</a></li>
+								<li><a href="#portfolio">Portfolio</a></li>
+								<li><a href="#contact">Contact</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
 	</body>
 </html>
