afbd3351fbdf136bd6a9feb6bb29b0415f8a874b
Tim Williams <tikytimbo5@yahoo.com>
2017-08-25 14:20:55 -0600

    offset and resized contact form

--- public_html/index.php
+++ public_html/index.php
@@ -56,7 +56,6 @@
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-right">
-								<li><a href="Home">Home</a></li>
 								<li><a href="#bio">Bio</a></li>
 								<li><a href="#portfolio">Portfolio</a></li>
 								<li><a href="#contact">Contact</a></li>
@@ -126,59 +125,65 @@
 		</section>
 
 		<!--contact form-->
-		<form id="contact" action="php/mailer.php" method="POST">
-			<div class="form-group">
-				<label for="name">Name <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-user" aria-hidden="true"></i>
-					</div>
-					<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="email">Email <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-envelope" aria-hidden="true"></i>
-					</div>
-					<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="subject">Subject</label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-pencil" aria-hidden="true"></i>
-					</div>
-					<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="message">Message <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-comment" aria-hidden="true"></i>
-					</div>
-					<textarea class="form-control" rows="5" id="message" name="message"
-								 placeholder="Message (2000 characters max)"></textarea>
-				</div>
-			</div>
+		<section id="contact">
+			<div class="row">
+				<div class="col-md-5 col-md-offset-1">
+					<form id="contact" action="php/mailer.php" method="POST">
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message"
+											 placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
 
-			<!-- Google reCAPTCHA -->
-			<div class="g-recaptcha" data-sitekey="----"></div>
+						<!-- Google reCAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="----"></div>
 
-			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-		</form>
+						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+					</form>
+				</div>
+			</div>
+		</section>
 
-			</main>
+		</main>
 
 		<!-- footer -->
-			<footer>
-				<div class="text-center">
-					<p class="foot-content">Copyright &copy; Timothy Williams 2017</p>
-				</div>
+		<footer>
+			<div class="text-center">
+				<p class="foot-content">Copyright &copy; Timothy Williams 2017</p>
+			</div>
 		</footer>
 
 
